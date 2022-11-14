dce0d27424265534873e936c93b7a47fca2d4d6c
A7med-Sharaf <115605059+A7med-sharaf@users.noreply.github.com>
2022-11-14 20:34:36 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 alert("hello")
 
-var typed = new  Typed('.type',{
-    strings: [
-    'Hello',' My name is Zouraiz.','I Love To Code.','I Love To Teach.'],
-    typeSpeed:100,
-    backSpeed:70,
-    loop:true,
-    });
+// var typed = new  Typed('.type',{
+//     strings: [
+//     'Hello',' My name is Zouraiz.','I Love To Code.','I Love To Teach.'],
+//     typeSpeed:100,
+//     backSpeed:70,
+//     loop:true,
+//     });
